c6ca3f36caf035aed4d769195d1f35eb8a2a0f03
Greg Roman <greg.roman@onepeloton.com>
2024-01-03 16:15:13 -0500

    remove comment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,6 @@ import java.time.Instant
 
 fun main(args: Array<String>) {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    // println("Program arguments: ${args.joinToString()}")
-
     val subscriptionRepository = SubscriptionRepository()
 
     // Create a new subscription.
